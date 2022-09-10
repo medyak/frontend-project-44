7cbcfd0ea54df6ec496f39425287049acebfb9ca
Mick Mednikov <the.cuprum@gmail.com>
2022-09-10 15:51:46 +0500

    predicate must return true/false

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,14 +5,13 @@ const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer
 
 const isPrime = (n) => {
   for (let i = 2; i < n; i += 1) {
-    if (n % i === 0) return 'no';
+    if (n % i === 0) return false;
   }
-  return 'yes';
+  return true;
 };
-
 const gameRoundData = () => {
   const getNumber = getRandomValue(1, 20);
-  const calculatedAnswer = isPrime(getNumber);
+  const calculatedAnswer = isPrime(getNumber) === true ? 'yes' : 'no';
 
   return [getNumber, calculatedAnswer];
 };

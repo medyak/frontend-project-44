efd60c25da55ce4d0bddafebe5253c9c77e151a8
Mick Mednikov <the.cuprum@gmail.com>
2022-09-13 21:49:58 +0500

    Force number 1 to a prime number?

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,7 +5,7 @@ const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer
 
 const isPrime = (n) => {
   for (let i = 2; i < n; i += 1) {
-    if (n < 2) return true;
+    if (n < 2) return false;
     if (n % i === 0) return false;
   }
   return true;

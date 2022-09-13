4171260ec34dd5fad3ce4a0cf0e8487b27131896
Mick Mednikov <the.cuprum@gmail.com>
2022-09-13 20:02:23 +0500

    Move function calculateValues from utils.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -1,9 +1,27 @@
 import _ from 'lodash';
 
 import startCalcGame from '../index.js';
-import { getRandomValue, calculateValues } from '../utils.js';
+import { getRandomValue } from '../utils.js';
 
 const gameDescription = 'What is the result of the expression?';
+
+const calculateValues = (operandOne, operandTwo, operator) => {
+  let result = 0;
+  switch (operator) {
+    case '+':
+      result = operandOne + operandTwo;
+      break;
+    case '-':
+      result = operandOne - operandTwo;
+      break;
+    case '*':
+      result = operandOne * operandTwo;
+      break;
+    default:
+      throw new Error('ERR: Uknown operation sign received');
+  }
+  return result;
+};
 
 const gameRoundData = () => {
   const operator = _.sample(['+', '-', '*']);
--- src/utils.js
+++ src/utils.js
@@ -3,24 +3,6 @@
 
 const getRandomValue = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-const calculateValues = (operandOne, operandTwo, operator) => {
-  let result = 0;
-  switch (operator) {
-    case '+':
-      result = operandOne + operandTwo;
-      break;
-    case '-':
-      result = operandOne - operandTwo;
-      break;
-    case '*':
-      result = operandOne * operandTwo;
-      break;
-    default:
-      throw new Error('ERR: Uknown operation sign received');
-  }
-  return result;
-};
-
 const isPrime = (n) => {
   for (let i = 2; i < n; i += 1) {
     if (n % i === 0) return false;
@@ -55,4 +37,4 @@ const getProgressionArray = (progressionLength, progressionFirstNumber, progress
   return progression;
 };
 
-export { getRandomValue, calculateValues, isPrime, getGCD, getProgressionArray };
+export { getRandomValue, isPrime, getGCD, getProgressionArray };

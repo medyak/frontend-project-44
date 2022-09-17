923ddb75522967212820a2d38d528d4d39501cf3
Mick Mednikov <the.cuprum@gmail.com>
2022-09-17 16:46:31 +0500

    Named the variables properly now

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,7 +4,7 @@ import { getRandomValue } from '../utils.js';
 
 const gameDescription = 'What number is missing in the progression?';
 
-const getProgressionArray = (length, firstNumber, step) => {
+const getProgression = (length, firstNumber, step) => {
   let nextElement = firstNumber;
   const progression = [nextElement];
 
@@ -20,12 +20,12 @@ const getRoundData = () => {
   const progressionLength = getRandomValue(5, 15);
   const progressionFirstNumber = getRandomValue();
   const progressionStep = getRandomValue(2, 5);
-  const progressionArray = getProgressionArray(progressionLength, progressionFirstNumber, progressionStep);
+  const aProgression = getProgression(progressionLength, progressionFirstNumber, progressionStep);
 
   const ItemIdToHide = getRandomValue(1, progressionLength - 1);
-  const answer = String(progressionArray[ItemIdToHide]);
-  progressionArray[ItemIdToHide] = '..';
-  const question = progressionArray.join(' ');
+  const answer = String(aProgression[ItemIdToHide]);
+  aProgression[ItemIdToHide] = '..';
+  const question = aProgression.join(' ');
 
   return [question, answer];
 };

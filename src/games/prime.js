76f127ba8ea2bdcbf761a5969fa8f5558ac9aba4
Mick Mednikov <the.cuprum@gmail.com>
2022-09-17 18:07:16 +0500

    kick hexlet actions script to work

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,14 +2,13 @@ import startPrimeGame from '../index.js';
 import { getRandomValue } from '../utils.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-
 const isPrime = (n) => {
   for (let i = 2; i < n; i += 1) {
     if (n % i === 0) return false;
   }
+
   return true;
 };
-
 const getRoundData = () => {
   const question = getRandomValue(1, 20);
   const answer = isPrime(question) ? 'yes' : 'no';
